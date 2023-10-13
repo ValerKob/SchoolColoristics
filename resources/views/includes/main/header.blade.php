d312cae592909192c08efe6f47ca8b8816a8a116
ValerKob <valer.kobelev2017@yandex.ru>
2023-10-13 08:59:45 +0300

    fvf

--- resources/views/includes/main/header.blade.php
+++ resources/views/includes/main/header.blade.php
@@ -1,5 +1,21 @@
 <!--================ Start Header =================-->
 <header>
-    
+    <div class="container">
+        <div class="headerLogo">
+            <img class="headerLogoImg" src="" alt="">
+        </div>
+        <ul class="headerUl">
+            <li class="headerLi">
+                <a class="headerLink" href="#">Для кого</a>
+            </li>
+        </ul>
+        <ul class="headerUl">
+            <li class="headerLi">
+                <a class="headerLink" href="#">
+                    <img class="" src="" alt="">
+                </a>
+            </li>
+        </ul>
+    </div>
 </header>
 <!--================ End Header =================-->
