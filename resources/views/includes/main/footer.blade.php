31983da567b5c6c8a69a15a7f26c1bede467547b
Val <89907932+ValerKob@users.noreply.github.com>
2023-10-14 13:08:18 +0300

    fvfv

--- resources/views/includes/main/footer.blade.php
+++ resources/views/includes/main/footer.blade.php
@@ -1,2 +1,45 @@
 <!--================ Start Footer =================-->
+<footer>
+    <div class="container">
+        <div class="wrapperFooter">
+            <div class="rowFooter">
+                <div class="colFooter">
+                    <div class="colFooterTilte">
+                        ШКОЛА
+                        КОЛОРИСТИКИ
+                    </div>
+                    <div class="colFooterSocial">
+                        <a class="headerLink headerLinkMrImg" href="#">
+                            <img class="headerLinkImg" src="./assets/images/social/vk.svg" alt="">
+                        </a>
+                        <a class="headerLink headerLinkMrImg" href="#">
+                            <img class="headerLinkImg" src="./assets/images/social/tg.svg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="colFooter">
+                    <div class="colFooterTilteType">
+                        Контакты
+                    </div>
+                    <div class="colFooterContent">
+                        <div class="colFooterText">Ежедневно: (с 9:00 до 20:00)</div>
+                        <div class="colFooterText">Тел: +7 (911) 875 44-99</div>
+                        <div class="colFooterText">Email: colorBeautyarh@gmail.ru</div>
+                    </div>
+                </div>
+                <div class="colFooter">
+                    <div class="colFooterTilteType">
+                        Страницы
+                    </div>
+                    <div class="colFooterContent pages">
+                        <a class="colFooterLink" href="#">Для&nbsp;кого</a>
+                        <a class="colFooterLink" href="#">О&nbsp;нас</a>
+                        <a class="colFooterLink" href="#">Курсы </a>
+                        <a class="colFooterLink" href="#">Информация</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 <!--================ End Footer =================-->
